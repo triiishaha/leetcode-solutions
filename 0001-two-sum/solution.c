813e6dc2a390c953c7c6f06f5604cdd7fec3e64c
Trisha Khattri <tkhattri07@gmail.com>
2025-10-03 17:20:38 +0000

    [LeetCode Sync] Runtime - 110 ms (8.57%), Memory - 8.9 MB (99.87%)

---
+++ 0001-two-sum/solution.c
@@ -0,0 +1,23 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
+{
+    int i,j;
+    *returnSize = 2;
+    int *num = (int*)malloc(2* sizeof(int));
+   for(i=0; i<numsSize; i++)
+   {
+    for(j=i+1; j<numsSize; j++)
+    {
+      if(nums[i]+nums[j]== target)
+     {
+        num[0] = i;
+        num[1] = j;
+     }
+    }
+     
+     
+   } 
+   return num;
+}
