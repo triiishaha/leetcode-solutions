b05095ad35f81675b7d9d64f348156c86d5c3658
Trisha Khattri <tkhattri07@gmail.com>
2025-10-08 18:19:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (100.00%)

---
+++ 0069-sqrtx/solution.c
@@ -0,0 +1,13 @@
+int mySqrt(int x) 
+{
+    int result;
+    if(x<0)
+    {
+        printf("Squareroot of negative no not given");
+    }  
+    else
+    {
+        result= sqrt(x);
+    } 
+    return result; 
+}
