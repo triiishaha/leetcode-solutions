0e6cbe8ce4b5d6f1f0f3be9ef6e1f23ecabcbca6
Trisha Khattri <tkhattri07@gmail.com>
2026-07-13 11:56:14 +0000

    [LeetCode Sync] Runtime - 5 ms (82.80%), Memory - 45.8 MB (86.73%)

---
+++ 0009-palindrome-number/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        int n,d,rev;
+        n=x;
+        rev = 0;
+
+
+        while(n>0)
+        {
+            d = n%10;
+            rev = (rev*10) + d;
+            n = n/10;
+        }
+        if(rev == x)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+       
+    }
+}
